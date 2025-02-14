ac9d66baf4a994fd7ee2d649abc9c2df8687afd6
Vivek Pruthi <VIVEKPRUTHI6172@GMAIL.COM>
2025-02-14 18:51:48 +0530

    Solution to Check Multiples

---
+++ Check Multiples@@6788dd63490637cfc44e1b9a/code.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    int a;
+    int b;
+    scanf("%d%d", &a,&b);
+    if(a%b==0)
+    {
+        printf("Yes");
+
+    }
+    else{
+        printf("No");
+
+    }
+    return 0;
+}
