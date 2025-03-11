e6b8b6a0bffa46a0fb0253edd39c0a829fa79845
Vivek Pruthi <VIVEKPRUTHI6172@GMAIL.COM>
2025-03-11 11:38:15 +0530

    Solution to Print Numbers from 1 to N

--- Print Numbers from 1 to N@@676c4885c18c9c17a0f50666/code.c
+++ Print Numbers from 1 to N@@676c4885c18c9c17a0f50666/code.c
@@ -1,54 +1,10 @@
-// #include<stdio.h>
-// int main(){
-//     int n;
-//     scanf("%d",&n);
-//     if( n>0){
-//         for (int i = 0 ; i >= n ; i++){
-//             printf("%d", i);
-//             if (i > n) {
-//             printf(" "); 
-//         }
-//         }
-    
-//     }
-//     else {
-//         for (int i = 0 ; i <= n ; i--){
-//             printf("%d", i);
-//             if (i < n) {
-//             printf(" "); 
-//     }
-// }
-//     }
-//     printf("\n");
-//     return 0;
-// }
 #include <stdio.h>
-
-int main() {
-    int N;
-
-    // Read the input
-    scanf("%d", &N);
-
-    // Check if N is positive or negative
-    if (N > 0) {
-        // Print numbers from 1 to N
-        for (int i = 1; i <= N; i++) {
-            printf("%d", i);
-            if (i < N) {
-                printf(" "); // Print space between numbers
-            }
-        }
-    } else {
-        // Print numbers from 1 to N (negative case)
-        for (int i = 1; i >= N; i--) {
-            printf("%d", i);
-            if (i > N) {
-                printf(" "); // Print space between numbers
-            }
-        }
+int main(){
+    int n;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+    for(int i = 1; i <= n; i++){
+       printf("%d ", i);
     }
-
-    printf("\n"); // New line at the end
     return 0;
 }
