e7509359bc6c80c1c073624d1205fc6fed86e65c
Vivek Pruthi <VIVEKPRUTHI6172@GMAIL.COM>
2025-03-27 20:23:28 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -1,10 +1,11 @@
 int fibonacciSeries(int n){
     int a=0, b=1;
+    int c;
     printf("0 1 ");
     for(int i=2 ;i<n;i++){
         a=b;
         b=c;
-        int c=a+b;
+        c=a+b;
         printf("%d",c);
     }
 }
